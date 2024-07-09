abfe5182edcaa373447a3162de78fcf03f86525f
Dima K <@users.noreply.github.com>
2024-07-09 14:27:37 +0200

    built a calculator

--- calc.py
+++ calc.py
@@ -1,12 +1,18 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
 import sys
 
-
 def main():
-    """Implement the calculator"""
-    pass  # YOUR CODE HERE
+    if sys.argv[2] == "+":
+        return float(sys.argv[1]) + float(sys.argv[3])
+    if sys.argv[2] == "-":
+        return float(sys.argv[1]) - float(sys.argv[3])
+    if sys.argv[2] == "*":
+        return float(sys.argv[1]) * float(sys.argv[3])
+    if sys.argv[2] == "/":
+        return float(sys.argv[1]) / float(sys.argv[3])
+    if sys.argv[2] == "%":
+        return float(sys.argv[1]) % float(sys.argv[3])
 
 
 if __name__ == "__main__":
     print(main())
-    
