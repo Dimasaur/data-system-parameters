9e84f42995d56128dd74f68efec48172ca52dea0
Dima K <@users.noreply.github.com>
2024-07-09 15:56:18 +0200

    Improved style

--- calc.py
+++ calc.py
@@ -1,7 +1,27 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
 import sys
 
+
 def main():
+    """
+    Perform basic arithmetic operations based on command-line arguments.
+
+    The script expects four command-line arguments:
+    1. The first number (float or int).
+    2. The operator (one of '+', '-', '*', '/', '%').
+    3. The second number (float or int).
+
+    Returns:
+        float: The result of the arithmetic operation.
+
+    Raises:
+        ValueError: If the operator is not one of '+', '-', '*', '/', '%'.
+        ZeroDivisionError: If division by zero is attempted.
+
+    Example:
+        $ python script.py 5 + 3
+        8.0
+    """
     if sys.argv[2] == "+":
         return float(sys.argv[1]) + float(sys.argv[3])
     if sys.argv[2] == "-":
